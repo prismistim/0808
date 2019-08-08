36525e75593350e999251392589599a2f2e6db9b
prismistim <prismistim@hotmail.com>
2019-08-08 13:06:58 +0900

    削除

--- src/pages/Happy.vue
+++ src/pages/Happy.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
   div
     h3.section-title はっぴー
-    p.description もしいただけるなら飛んで喜びます。
-    .happy-item
-      h4.sub-section-title 欲しいものタグ
-      p.description あなたの「こんなのが欲しい」的な個人的な要望から理想までハッシュタグ
-        span.wish-tag #stanon_wishlist
-        | でつぶやいてください。なんか作るときに参考にしたいです。
-      .btn-field
-        a.link-btn.twitter(href="https://twitter.com/search?q=%23stanon_wishlist" target="blank") Twitter
-    .happy-item
-      h4.sub-section-title bandcamp
-      p.description Wishlistにいつか買おうと思ってた音楽を溜めすぎました...。1曲で超幸せなのでもしよろしければギフトでください。
-      .btn-field
-        a.link-btn.bandcamp(href="https://bandcamp.com/snowsphere/wishlist" target="blank") bandcamp
+    p.description これからもどうぞよろしくお願いします
+    //- .happy-item
+    //-   h4.sub-section-title 欲しいものタグ
+    //-   p.description あなたの「こんなのが欲しい」的な個人的な要望から理想までハッシュタグ
+    //-     span.wish-tag #stanon_wishlist
+    //-     | でつぶやいてください。なんか作るときに参考にしたいです。
+    //-   .btn-field
+    //-     a.link-btn.twitter(href="https://twitter.com/search?q=%23stanon_wishlist" target="blank") Twitter
+    //- .happy-item
+    //-   h4.sub-section-title bandcamp
+    //-   p.description Wishlistにいつか買おうと思ってた音楽を溜めすぎました...。1曲で超幸せなのでもしよろしければギフトでください。
+    //-   .btn-field
+    //-     a.link-btn.bandcamp(href="https://bandcamp.com/snowsphere/wishlist" target="blank") bandcamp
 </template>
